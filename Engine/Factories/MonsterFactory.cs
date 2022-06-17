02fce44d2837e76ae874ecc9af9571a78c84c4dd
crack <modzskull@gmail.com>
2022-06-17 05:42:03 -0600

    updated

--- Engine/Factories/MonsterFactory.cs
+++ Engine/Factories/MonsterFactory.cs
@@ -4,27 +4,25 @@ namespace Engine.Factories
 {
     public static class MonsterFactory
     {
-        private static object RandomNumberGenerator;
-
         public static Monster GetMonster(int monsterID)
         {
             switch (monsterID)
             {
                 case 1:
                     Monster snake =
-                        new Monster("Snake", "Snake.png", 4, 4, 5, 1);
+                        new Monster("Snake", "Snake.png", 4, 4, 1, 2, 5, 1);
                     AddLootItem(snake, 9001, 25);
                     AddLootItem(snake, 9002, 75);
                     return snake;
                 case 2:
                     Monster rat =
-                        new Monster("Rat", "Rat.png", 5, 5, 5, 1);
+                        new Monster("Rat", "Rat.png", 5, 5, 1, 2, 5, 1);
                     AddLootItem(rat, 9003, 25);
                     AddLootItem(rat, 9004, 75);
                     return rat;
                 case 3:
                     Monster giantSpider =
-                        new Monster("Giant Spider", "GiantSpider.png", 10, 10, 10, 3);
+                        new Monster("Giant Spider", "GiantSpider.png", 10, 10, 1, 4, 10, 3);
                     AddLootItem(giantSpider, 9005, 25);
                     AddLootItem(giantSpider, 9006, 75);
                     return giantSpider;
